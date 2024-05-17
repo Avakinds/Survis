c7ced0f9cd0e3a907d03af2254018fd9ba4bfec0
Avakinds <115653088+Avakinds@users.noreply.github.com>
2024-05-17 09:02:54 +0100

    Add files via upload

---
+++ survis-master/src/data/generated/available_img.js
@@ -0,0 +1,1 @@
+const availableImg = ["Aguilera2015OliveClassification","Bird2022AffectiveComputing","Dandil2019DistanceMeasurement","Ferreyra2018VisualImpairment","Shaqwi2021DeepLearning","Tao2012SeafoodQuality","Yang2021FruitPackaging"];
